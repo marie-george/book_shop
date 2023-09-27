4f474e3b6f34806e69ab5eb56295d6f2d641b92d
marie-george <maria85night@gmail.com>
2023-09-28 00:39:18 +0300

    fixed author field in Book model

--- models_app/models/models.py
+++ models_app/models/models.py
@@ -26,7 +26,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100, verbose_name='заголовок')
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True, verbose_name='автор')
+    author = models.ManyToManyField(Author, on_delete=models.CASCADE, null=True, blank=True, verbose_name='автор')
     description = models.TextField(verbose_name='описание')
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE, null=True, blank=True, verbose_name='жанр')
     price = models.IntegerField(default=0, verbose_name='цена')
